1e67638b7c61e93abefa39538f3810c4193ede87
Kirill <evstratovkirill@yandex.ru>
2020-07-20 09:47:46 +0300

    подписки, отписки, лайки, комментарии по хештегам. Через некоторое время банят))

---
+++ InstagramBot/InstagramBotForm.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace InstagramBot
+{
+    partial class InstagramBotForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.login_txt = new System.Windows.Forms.TextBox();
+            this.password_txt = new System.Windows.Forms.TextBox();
+            this.login_lbl = new System.Windows.Forms.Label();
+            this.password_lbl = new System.Windows.Forms.Label();
+            this.login_btn = new System.Windows.Forms.Button();
+            this.logBox = new System.Windows.Forms.ListBox();
+            this.chk_boxLike = new System.Windows.Forms.CheckBox();
+            this.chk_boxComment = new System.Windows.Forms.CheckBox();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.lb_ToDo = new System.Windows.Forms.Label();
+            this.actoinsCount_txt = new System.Windows.Forms.TextBox();
+            this.lb_actionsCount = new System.Windows.Forms.Label();
+            this.lb_unfollow = new System.Windows.Forms.Label();
+            this.btn_unfollow = new System.Windows.Forms.Button();
+            this.chk_follow = new System.Windows.Forms.CheckBox();
+            this.pictureBoxInst = new System.Windows.Forms.PictureBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxInst)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // login_txt
+            // 
+            this.login_txt.Location = new System.Drawing.Point(67, 29);
+            this.login_txt.Name = "login_txt";
+            this.login_txt.Size = new System.Drawing.Size(100, 20);
+            this.login_txt.TabIndex = 0;
+            // 
+            // password_txt
+            // 
+            this.password_txt.Location = new System.Drawing.Point(67, 56);
+            this.password_txt.Name = "password_txt";
+            this.password_txt.PasswordChar = '*';
+            this.password_txt.Size = new System.Drawing.Size(100, 20);
+            this.password_txt.TabIndex = 1;
+            // 
+            // login_lbl
+            // 
+            this.login_lbl.AutoSize = true;
+            this.login_lbl.Location = new System.Drawing.Point(13, 29);
+            this.login_lbl.Name = "login_lbl";
+            this.login_lbl.Size = new System.Drawing.Size(36, 13);
+            this.login_lbl.TabIndex = 2;
+            this.login_lbl.Text = "Login:";
+            // 
+            // password_lbl
+            // 
+            this.password_lbl.AutoSize = true;
+            this.password_lbl.Location = new System.Drawing.Point(10, 62);
+            this.password_lbl.Name = "password_lbl";
+            this.password_lbl.Size = new System.Drawing.Size(56, 13);
+            this.password_lbl.TabIndex = 3;
+            this.password_lbl.Text = "Password:";
+            // 
+            // login_btn
+            // 
+            this.login_btn.Location = new System.Drawing.Point(67, 83);
+            this.login_btn.Name = "login_btn";
+            this.login_btn.Size = new System.Drawing.Size(75, 23);
+            this.login_btn.TabIndex = 4;
+            this.login_btn.Text = "Log In";
+            this.login_btn.UseVisualStyleBackColor = true;
+            this.login_btn.Click += new System.EventHandler(this.login_btn_Click);
+            // 
+            // logBox
+            // 
+            this.logBox.FormattingEnabled = true;
+            this.logBox.Location = new System.Drawing.Point(193, 29);
+            this.logBox.Name = "logBox";
+            this.logBox.Size = new System.Drawing.Size(307, 82);
+            this.logBox.TabIndex = 5;
+            // 
+            // chk_boxLike
+            // 
+            this.chk_boxLike.AutoSize = true;
+            this.chk_boxLike.Checked = true;
+            this.chk_boxLike.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chk_boxLike.Location = new System.Drawing.Point(55, 208);
+            this.chk_boxLike.Name = "chk_boxLike";
+            this.chk_boxLike.Size = new System.Drawing.Size(82, 17);
+            this.chk_boxLike.TabIndex = 6;
+            this.chk_boxLike.Text = "Like Photos";
+            this.chk_boxLike.UseVisualStyleBackColor = true;
+            // 
+            // chk_boxComment
+            // 
+            this.chk_boxComment.AutoSize = true;
+            this.chk_boxComment.Checked = true;
+            this.chk_boxComment.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chk_boxComment.Location = new System.Drawing.Point(55, 232);
+            this.chk_boxComment.Name = "chk_boxComment";
+            this.chk_boxComment.Size = new System.Drawing.Size(99, 17);
+            this.chk_boxComment.TabIndex = 7;
+            this.chk_boxComment.Text = "Comment Posts";
+            this.chk_boxComment.UseVisualStyleBackColor = true;
+            // 
+            // btn_start
+            // 
+            this.btn_start.Enabled = false;
+            this.btn_start.Location = new System.Drawing.Point(55, 274);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(75, 23);
+            this.btn_start.TabIndex = 8;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // lb_ToDo
+            // 
+            this.lb_ToDo.AutoSize = true;
+            this.lb_ToDo.Location = new System.Drawing.Point(55, 189);
+            this.lb_ToDo.Name = "lb_ToDo";
+            this.lb_ToDo.Size = new System.Drawing.Size(103, 13);
+            this.lb_ToDo.TabIndex = 9;
+            this.lb_ToDo.Text = "Choose your actions";
+            // 
+            // actoinsCount_txt
+            // 
+            this.actoinsCount_txt.Location = new System.Drawing.Point(170, 153);
+            this.actoinsCount_txt.Name = "actoinsCount_txt";
+            this.actoinsCount_txt.Size = new System.Drawing.Size(41, 20);
+            this.actoinsCount_txt.TabIndex = 11;
+            this.actoinsCount_txt.Text = "20";
+            // 
+            // lb_actionsCount
+            // 
+            this.lb_actionsCount.AutoSize = true;
+            this.lb_actionsCount.Location = new System.Drawing.Point(12, 156);
+            this.lb_actionsCount.Name = "lb_actionsCount";
+            this.lb_actionsCount.Size = new System.Drawing.Size(155, 13);
+            this.lb_actionsCount.TabIndex = 12;
+            this.lb_actionsCount.Text = "Number of actions per hashtag:";
+            // 
+            // lb_unfollow
+            // 
+            this.lb_unfollow.AutoSize = true;
+            this.lb_unfollow.Location = new System.Drawing.Point(258, 153);
+            this.lb_unfollow.Name = "lb_unfollow";
+            this.lb_unfollow.Size = new System.Drawing.Size(242, 13);
+            this.lb_unfollow.TabIndex = 13;
+            this.lb_unfollow.Text = "Unsubscribe from everyone in the file: Unfollow.txt";
+            // 
+            // btn_unfollow
+            // 
+            this.btn_unfollow.Enabled = false;
+            this.btn_unfollow.Location = new System.Drawing.Point(261, 170);
+            this.btn_unfollow.Name = "btn_unfollow";
+            this.btn_unfollow.Size = new System.Drawing.Size(75, 23);
+            this.btn_unfollow.TabIndex = 14;
+            this.btn_unfollow.Text = "Unfollow";
+            this.btn_unfollow.UseVisualStyleBackColor = true;
+            this.btn_unfollow.Click += new System.EventHandler(this.btn_unfollow_Click);
+            // 
+            // chk_follow
+            // 
+            this.chk_follow.AutoSize = true;
+            this.chk_follow.Checked = true;
+            this.chk_follow.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chk_follow.Location = new System.Drawing.Point(55, 256);
+            this.chk_follow.Name = "chk_follow";
+            this.chk_follow.Size = new System.Drawing.Size(56, 17);
+            this.chk_follow.TabIndex = 15;
+            this.chk_follow.Text = "Follow";
+            this.chk_follow.UseVisualStyleBackColor = true;
+            // 
+            // pictureBoxInst
+            // 
+            this.pictureBoxInst.Location = new System.Drawing.Point(236, 208);
+            this.pictureBoxInst.Name = "pictureBoxInst";
+            this.pictureBoxInst.Size = new System.Drawing.Size(320, 320);
+            this.pictureBoxInst.TabIndex = 16;
+            this.pictureBoxInst.TabStop = false;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 303);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(218, 23);
+            this.progressBar.TabIndex = 17;
+            // 
+            // InstagramBotForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(586, 548);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.pictureBoxInst);
+            this.Controls.Add(this.chk_follow);
+            this.Controls.Add(this.btn_unfollow);
+            this.Controls.Add(this.lb_unfollow);
+            this.Controls.Add(this.lb_actionsCount);
+            this.Controls.Add(this.actoinsCount_txt);
+            this.Controls.Add(this.lb_ToDo);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.chk_boxComment);
+            this.Controls.Add(this.chk_boxLike);
+            this.Controls.Add(this.logBox);
+            this.Controls.Add(this.login_btn);
+            this.Controls.Add(this.password_lbl);
+            this.Controls.Add(this.login_lbl);
+            this.Controls.Add(this.password_txt);
+            this.Controls.Add(this.login_txt);
+            this.Name = "InstagramBotForm";
+            this.Text = "InstagramBot";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.InstagramBotForm_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxInst)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox login_txt;
+        private System.Windows.Forms.TextBox password_txt;
+        private System.Windows.Forms.Label login_lbl;
+        private System.Windows.Forms.Label password_lbl;
+        private System.Windows.Forms.Button login_btn;
+        private System.Windows.Forms.ListBox logBox;
+        private System.Windows.Forms.CheckBox chk_boxLike;
+        private System.Windows.Forms.CheckBox chk_boxComment;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Label lb_ToDo;
+        private System.Windows.Forms.TextBox actoinsCount_txt;
+        private System.Windows.Forms.Label lb_actionsCount;
+        private System.Windows.Forms.Label lb_unfollow;
+        private System.Windows.Forms.Button btn_unfollow;
+        private System.Windows.Forms.CheckBox chk_follow;
+        private System.Windows.Forms.PictureBox pictureBoxInst;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
---
+++ InstagramBot/InstagramBotForm.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using InstaSharper.API;
+using InstaSharper.API.Builder;
+using InstaSharper.Classes;
+using InstaSharper.Logger;
+using InstaSharper.Classes.Models;
+using System.IO;
+using System.Diagnostics;
+
+namespace InstagramBot
+{
+    public partial class InstagramBotForm : Form
+    {
+        private string login, password = string.Empty;
+
+        private Logger logger;
+        // мы
+        private static UserSessionData user;
+        private static IInstaApi api;
+        // массив хэштегов
+        private string[] hashtagArr;
+        // массив комментариев
+        private string[] commentsArr;
+        // список Pk пользователей
+        private List<string> folowingUsers;
+        // используется для чтения разом всех Pk из файла followers.txt
+        private string[] unfollowUsersAsrr;
+        // сколько лайков/комментариев/подписок на каждый хэштег
+        private int actionCount;
+        //private Stopwatch stopwatch;
+        public InstagramBotForm()
+        {
+            InitializeComponent();
+        }
+
+        private void login_btn_Click(object sender, EventArgs e)
+        {
+            login = login_txt.Text;
+            password = password_txt.Text;
+            logger = new Logger();
+
+            if (login == "" || password == "")
+                MessageBox.Show("Input login and password");
+            user = new UserSessionData();
+            user.UserName = login;
+            user.Password = password;
+
+            Login();
+        }
+
+        private async void btn_start_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                logBox.Items.Add("Getting started...");
+                //stopwatch = Stopwatch.StartNew();
+                progressBar.Value = 0;
+                folowingUsers = new List<string>();
+
+                actionCount = Convert.ToInt32(actoinsCount_txt.Text);
+                //logBox.Items.Add($"{actionCount} operations will be performed");
+                if (chk_boxComment.Checked)
+                {
+                    // считываем все строки в строковый массив
+                    commentsArr = File.ReadAllLines("Sources\\comments.txt");
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.CloseLog();
+                MessageBox.Show(ex.Message);
+            }
+            // чтение файла с хештегами
+            using (FileStream fstream = File.OpenRead("Sources\\hashtags.txt"))
+            {
+                byte[] array = new byte[fstream.Length];
+                fstream.Read(array, 0, array.Length);
+                string textFromFile = Encoding.Default.GetString(array);
+                // разбиваем текст по отдельным ключевым словам
+                TextToHashtagArr(textFromFile);  
+            }
+        }
+        private void TextToHashtagArr(string text)
+        {
+            hashtagArr = text.Split('#');
+            progressBar.Maximum = (hashtagArr.Length - 1) * actionCount;
+            foreach (string tag in hashtagArr)
+            {
+                if (tag != "")
+                {
+                    ProcessHashtag(tag);
+                }
+            }
+        }
+        private async void ProcessHashtag(string tag)
+        {
+            try {
+                // ищем информацию о теге
+                var hashtagInfo = await api.GetHashtagInfo(tag);
+                if (hashtagInfo.Succeeded)
+                {
+                    //logBox.Items.Add("Hashtag: #" + hashtagInfo.Value.Name + ", media count = " + hashtagInfo.Value.MediaCount);
+                    // получаем ленту с постами по хештегу
+                    var tagFeed = await api.GetTagFeedAsync(hashtagInfo.Value.Name, PaginationParameters.MaxPagesToLoad(5));
+                    int count = actionCount < (tagFeed.Value.Medias.Count) ? actionCount : tagFeed.Value.Medias.Count;
+                    foreach (var media in tagFeed.Value.Medias.Take(count))
+                    {
+                        if (chk_boxLike.Checked)
+                        {
+                            // ставим лайки каждому посту
+                            var likeResult = await api.LikeMediaAsync(media.InstaIdentifier);
+                            var resultString = likeResult.Value ? "liked" : "not liked";
+                            if (media.Images.Count > 0)
+                                logger.WriteLog($"Hashtag: #{hashtagInfo.Value.Name}, URI:{media.Images[0].URI}");
+                            logger.WriteLog($" Post URI: https://www.instagram.com/p/{media.Code}");
+                            logger.WriteLog($"User: {media.User.UserName} {resultString}");
+                        }
+                        if (chk_follow.Checked)
+                        {
+                            // подписываемся на автора картинки
+                            var followResult = await api.FollowUserAsync(media.User.Pk);
+                            var resultString = followResult.Succeeded ? "followed" : "not folowwed";
+                            logger.WriteLog($"{resultString} {media.User.UserName}");
+                            folowingUsers.Add(media.User.Pk.ToString());
+                        }
+                        if (chk_boxComment.Checked && commentsArr.Length > 0)
+                        {
+                            // ставим случайный комментарий автору фотки
+                            Random rnd = new Random();
+                            int randIndx = rnd.Next(0, commentsArr.Length - 1);
+                            string comment = commentsArr[randIndx];
+
+                            var commentResult = await api.CommentMediaAsync(media.InstaIdentifier, comment);
+                            var resultString = commentResult.Succeeded ? "comented" : "not comented";
+                            logger.WriteLog($"{resultString}");
+                            logger.WriteLog($"-------------------------------------------------------------");
+
+                        }
+                        if (media.Images.Count > 1)
+                            PrintImage(media.Images[1].URI);
+                        progressBar.Increment(1);
+                    }
+                }
+            } catch (Exception ex)
+            {
+                logger.CloseLog();
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private async void Login()
+        {
+            try
+            {
+                // создаем  обьект, через который происходит основная работа
+                api = InstaApiBuilder.CreateBuilder()
+                    .SetUser(user)
+                    .UseLogger(new DebugLogger(LogLevel.Exceptions))
+                    .SetRequestDelay(RequestDelay.FromSeconds(7, 9))
+                    .Build();
+                var loginRequest = await api.LoginAsync();
+                if (loginRequest.Succeeded)
+                {
+                    logBox.Items.Add("Logged in succes, press Start");
+                    btn_start.Enabled = true;
+                    btn_unfollow.Enabled = true;
+                }
+                else
+                {
+                    logBox.Items.Add("Logged in failed! " + loginRequest.Info.Message);
+                }
+            }
+            catch (Exception ex) {
+                logger.CloseLog();
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        private async void PrintImage(string uri)
+        {
+            try {
+                await Task.Run(() =>
+                {
+                    if (uri != "")
+                        pictureBoxInst.Load(uri);
+                    else
+                        pictureBoxInst.Image = null;
+                });
+            }
+            catch(Exception ex) {
+                WritePkToFile();
+                pictureBoxInst.Image = null;
+                //logger.CloseLog();
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        // запись в файд Pk пользователей на которых подписались
+        private void WritePkToFile()
+        {
+            using (StreamWriter sw = new StreamWriter("Sources\\followers.txt", append:true))
+            {
+                if (folowingUsers == null || folowingUsers.Count == 0)
+                    return;
+                foreach (string line in folowingUsers)
+                    sw.WriteLine(line);
+            }
+        }
+
+        private async void btn_unfollow_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                logBox.Items.Add($"Start unfollowing...");
+                // дополняем файл, если только  после запуска приложения были новые подписки
+                WritePkToFile();
+
+                unfollowUsersAsrr = File.ReadAllLines("Sources\\followers.txt");
+                int success = 0;
+                int failed = 0;
+                foreach (string pk in unfollowUsersAsrr)
+                {
+                    // отписка
+                    var unfollowResult = await api.UnFollowUserAsync(long.Parse(pk));
+
+                    if (unfollowResult.Succeeded)
+                        success++;
+                    else
+                        failed++;
+                }
+                logBox.Items.Add($"Unfollowing complited. Successfully: { success}. Failed: {failed}.");
+                logger.WriteLog($"Unfollowing complited. Successfully: { success}. Failed: {failed}.");
+                if (failed == 0)
+                    using (FileStream fs = File.Create("Sources\\followers.txt")) { }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                logger.CloseLog();
+            }
+            // чистим файл
+        }
+
+        private void InstagramBotForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            WritePkToFile();
+            logger.CloseLog();
+        }
+    }
+
+    public class Logger
+    {
+        public StreamWriter file;
+        private string filename;
+        public Logger()
+        {
+            filename = DateTime.Now.ToString("yyyy-MM-dd H.mm.ss");
+            file = new StreamWriter($"Logs\\{filename}.txt", append: true);
+        }
+        ~Logger()
+        {
+            CloseLog();
+        }
+        public void WriteLog(string text)
+        {
+            file.WriteLine(text);
+        }
+        public void CloseLog()
+        {
+            file.Close();
+        }
+    }
+}
